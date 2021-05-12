85203668e98d8dec7ff58cfbcd5bb46d0de9168f
sai <saihtun1430@gmail.com>
2021-05-12 10:24:59 -0700

    :wink: added thankyou route

--- pages/thankyou.jsx
+++ pages/thankyou.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
 export default function Thankyou() {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/")
+        }, 2000);
+    }, [])
+
     return (
         <Container>
-            <h1>Thanks for your message, i'll be right back to you shortly!</h1>
+            <h2>Thanks for your message, i'll be right back to you shortly!</h2>
         </Container>
     )
 }
 
 
-const Container = styled.div``;
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
